0f496566a09b7108686c8e869f195b4cd0172a5b
Moni2352 <marcus.labrecque@aspengroveschool.com>
2026-02-03 09:11:55 -0800

    Initial

---
+++ Emulator/Program.cs
@@ -0,0 +1,29 @@
+﻿
+internal class Program
+{
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        // Dictionaries of Hex commands and their purpose and command structure (# of bytes after for arguments)
+        
+        OpenFileDialog dialog = new OpenFileDialog
+        {
+            Title = "Select a file to load",
+            InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+            Filter = "All Files (*.*)|*.*",
+            Multiselect = false
+        };
+
+        if (dialog.ShowDialog() == DialogResult.OK)
+        {
+            string filepath = dialog.FileName;
+            Console.WriteLine($"Selected File: {filepath}");
+        }
+        else
+        {
+            Console.WriteLine("No File Selected");
+        }
+
+        Console.ReadKey();
+    }
+}
